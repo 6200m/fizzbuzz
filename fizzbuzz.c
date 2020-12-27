d185a0755b5ab43bf99951822b4cc73e3e7120a0
Gloom <76667151+6200m@users.noreply.github.com>
2020-12-27 16:44:11 -0600

    Version 1.0.1.1.111

---
+++ fizzbuzz.c
@@ -0,0 +1,25 @@
+/*Resources used:
+https://www.tutorialspoint.com/cprogramming/c_variables.htm
+https://www.google.com/search?q=elif+c&rlz=1C1CHBD_enUS895US895&oq=elif+c&aqs=chrome..69i57.783j0j1&sourceid=chrome&ie=UTF-8
+https://www.google.com/search?
+q=printf+new+line&rlz=1C1CHBD_enUS89505895&oq=printf+new+line&aqs=chrome..69i57.1891j0j1&sourceid=chrome&ie=UTF-8
+*/
+
+#include <stdio.h>
+#include<iostream>
+int main(null)
+  using namespace std;
+  #include <rips/stdio.h>
+  #include <includes/constants.h>
+  #include <includes/null.h>
+  #include <includes/static.h>
+  execute();
+int execute_1(null)
+  printf(/n)
+  x + 1; printf(x); check();
+int check(null)
+  if x > 100
+    exit(null);
+    return 0;
+  elif x < 100
+    main(null);
